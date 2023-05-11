bc263fd73f62e9394cccae47893755f3f4c9d661
Zahraa Bou Zeid <zahraahbouzeid@gmail.com>
2023-05-11 22:42:28 +0200

    Commit

--- RefactoringTool.py
+++ RefactoringTool.py
@@ -25,20 +25,20 @@ for Line in tqdm(fixInsight):
         getLine = getLine[getLine.index(".") + 1 : getLine.index(")")]
         changeLine = getLine.replace(uParameter, cParameter)
         print (f"Replacing {getLine} with {changeLine}")
-        with open(currentUnit, 'r+', encoding='utf8') as fHandle:
+        with open(currentUnit, 'r+', encoding = "utf8") as fHandle:
             base = fHandle.read()
             changed = base.replace(getLine, changeLine)
             fHandle.seek(0)
             fHandle.write(changed)
-            fHandle.truncate()
             fHandle.close()
             linecache.clearcache()
     except:
         print(colored(f"Error in {uName}", "red"))
         lst.append(uName)
 
-print("\nReplacing finished with", colored(f"Errors in {len(lst)} Unit(s):", "red"))
-
 if len(lst) > 0:
+    print("\nReplacing finished with", colored(f"Errors in {len(lst)} Unit(s):", "red"))
     for _ in lst:
         print(colored(_ , "red"))
+else:
+    print(colored("\nReplacing done successfully.", "green"))
