c5ca0a56c30afdd86253a3862552f1167089bbd7
Zahraa Bou Zeid <zahraahbouzeid@gmail.com>
2023-05-13 15:14:25 +0200

    Commit

--- RefactoringTool.py
+++ RefactoringTool.py
@@ -6,8 +6,11 @@ fixInsight = open(r"C:\Users\zahra\Documents\GitHub\WWRefactoringTool\FixInsight
 print('Reading FixInsight Log: ')
 lst = []
 dic = {}
+linesCount = 0
+errorsCount = 0
 
 for Line in tqdm(fixInsight):
+    linesCount += 1
     Line = Line.strip().split(" ")
     uName = Line[2][:Line[2].index("(")]
     uLine = int(Line[2][Line[2].index("(") + 1 :Line[2].index(")")])
@@ -20,9 +23,17 @@ for Line in tqdm(fixInsight):
 
     uParameter = uParameter + ": string"
     cParameter = "const " + uParameter
+
     try:
         getLine = linecache.getline(currentUnit, uLine)
-        getLine = getLine[getLine.index(".") + 1 : getLine.index(uParameter) + len(uParameter)]
+        try:
+            getLine = getLine[getLine.index(".") + 1 : getLine.index(uParameter) + len(uParameter)]
+        except:
+            try:
+                getLine = getLine[getLine.index("(") + 1 : getLine.index(uParameter) + len(uParameter)]
+            except:
+                getLine = getLine[: getLine.index(uParameter) + len(uParameter)]
+
         changeLine = getLine.replace(uParameter, cParameter)
         with open(currentUnit, 'r+', encoding = "utf8") as fHandle:
             base = fHandle.read()
@@ -41,6 +52,11 @@ for Line in tqdm(fixInsight):
     except:
         print(colored(f"Error in {uName}", "red"))
         lst.append(uName)
+        errorsCount += 1
+
+successCount = linesCount - errorsCount
+successRate =  (successCount * 100) / linesCount
+fSuccessRate = "{:.2f}".format(successRate)
 
 if len(lst) > 0:
     print("\nReplacing finished with", colored(f"Errors in {len(lst)} Unit(s)", "red"))
@@ -54,5 +70,14 @@ if len(dic) > 0:
     print("You must check these Units: ")
     for key, value in dic.items():
         print(colored(f"{key} : {value}", "yellow"))
+
+print(colored(f"\n{successCount} of {linesCount} successfull changes made", "green"))
+
+if successRate > 70:
+    print(colored(f"{fSuccessRate}% Success Rate", "green"))
+elif 40 < successRate < 70:
+    print(colored(f"{fSuccessRate}% Success Rate", "yellow"))
+elif successRate < 40:
+    print(colored(f"{fSuccessRate}% Success Rate", "red"))
 
 input("\nPress Enter to exit...")
